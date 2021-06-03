815544fa45ea72df6312317241069ca14d8c9cd9
nithyavishnu21 <85152874+nithyavishnu21@users.noreply.github.com>
2021-06-03 18:48:10 +0530

    Add files via upload

---
+++ src/test/java/apiEngine/model/response/PostResponse.java
@@ -0,0 +1,13 @@
+package apiEngine.model.response;
+
+public class PostResponse {
+
+	public Integer userId;
+	public Integer id;
+	public String title;
+	public String body;
+	public Integer postId;
+	public String email;
+	public String name;
+
+}
